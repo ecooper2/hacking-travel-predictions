1d505d12f68d710281dd8d8149af29ef803ef8dd
Andrew Collier <andrew@acollier.com>
2014-11-01 13:51:41 -0400

    add aws upload script

---
+++ upload.sh
@@ -0,0 +1,3 @@
+aws s3 cp update/similar_dow.json s3://traffichackers/data/predictions/similar_dow.json --region us-east-1
+aws s3 cp update/similar_weekdays.json s3://traffichackers/data/predictions/similar_weekday.json --region us-east-1
+aws s3 cp update/similar_weekends.json s3://traffichackers/data/predictions/similar_weekend.json --region us-east-1
