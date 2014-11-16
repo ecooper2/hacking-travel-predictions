20e1d0c0c111444ca91f295e42b2c246defd64b7
Andrew Collier <andrew@acollier.com>
2014-11-16 16:15:09 -0500

    simplify code and add back archiving

--- upload.sh
+++ upload.sh
@@ -1,4 +1,6 @@
 # Run the Models
+FILENAME=`date +20%y_%m%d_%H%M`
+
 python ./BlueToadAnalysis.py today 'similar_dow.json' -w -t & > similar_dow_log.json
 if [[ $(date +%u) -gt 5 ]] ; then
   python ./BlueToadAnalysis.py weekend 'similar_weekends.json' -w -t & > similar_weekends_log.json
@@ -8,22 +10,18 @@ fi
 wait
 echo "model runs complete"
 
-# Compress the Model Outputs
-gzip -9 --keep --force update/similar_dow.json &
+# Compress, Archive, and Upload the Model Outputs
+gzip -9 --force update/similar_dow.json
+mv update/similar_dow.json.gz update/similar_dow_$FILENAME.json.gz
+aws s3 cp update/similar_dow_$FILENAME.json.gz s3://www.traffichackers.com/data/predictions/similar_dow.json --region us-east-1 --content-encoding gzip --content-type application/json &
 if [[ $(date +%u) -gt 5 ]] ; then
-  gzip -9 --keep --force update/similar_weekends.json &
+  gzip -9 --force update/similar_weekends.json
+  mv update/similar_weekends.json.gz update/similar_weekends_$FILENAME.json.gz
+  aws s3 cp update/similar_weekends_$FILENAME.json.gz s3://www.traffichackers.com/data/predictions/similar_weekends.json --region us-east-1 --content-encoding gzip --content-type application/json &
 else
-  gzip -9 --keep --force update/similar_weekdays.json &
+  gzip -9 --force update/similar_weekdays.json
+  mv update/similar_weekdays.json.gz update/similar_weekdays_$FILENAME.json.gz
+  aws s3 cp update/similar_weekdays_$FILENAME.json.gz  s3://www.traffichackers.com/data/predictions/similar_weekdays.json --region us-east-1 --content-encoding gzip --content-type application/json &
 fi
 wait
-echo "compression complete"
-
-# Upload to the Amazon Web Services S3 Store
-aws s3 cp update/similar_dow.json.gz s3://www.traffichackers.com/data/predictions/similar_dow.json --region us-east-1 --content-encoding gzip --content-type application/json &
-if [[ $(date +%u) -gt 5 ]] ; then
-  aws s3 cp update/similar_weekends.json.gz s3://www.traffichackers.com/data/predictions/similar_weekends.json --region us-east-1 --content-encoding gzip --content-type application/json &
-else
-  aws s3 cp update/similar_weekdays.json.gz  s3://www.traffichackers.com/data/predictions/similar_weekdays.json --region us-east-1 --content-encoding gzip --content-type application/json &
-fi
-wait
-echo "aws s3 upload complete"
+echo "compression, archiving, and upload complete"
